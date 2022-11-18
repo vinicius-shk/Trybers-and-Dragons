aa236ca45c9132567f1d70af92e235ac27581d51
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-11-18 16:21:56 -0300

    Conclusão do requisito 5

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,33 @@
+import Archetype from './Archetype';
+import IEnergy, { EnergyType } from '../Energy';
+
+export default class Warrior extends Archetype implements IEnergy {
+  static createdInstances = 0;
+  type_: EnergyType = 'stamina';
+  private _amount: number;
+  constructor(name: string) {
+    super(name);
+    Warrior.createdInstances += 1;
+    this._amount = this.cost;
+  }
+
+  public get type(): EnergyType {
+    return this.type_;
+  }
+  
+  public get energyType(): EnergyType {
+    return this.type;
+  }
+
+  public get amount(): number {
+    return this._amount;
+  }
+
+  public set amount(value: number) {
+    this._amount = value;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Warrior.createdInstances;
+  }
+}
